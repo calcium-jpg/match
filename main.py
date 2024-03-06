9f691cdc195dc4d442b003eee9b30e948f266e83
calcium-jpg <calvinthompson07@protonmail.com>
2024-03-05 21:54:04 -0600

    add command line arguments

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from match import match
 import json
 import numpy
+import sys
+
+from match import match
 
 sequence = [1,2,3,4,5,6,7,8,9,0]
 random_numbers = []
     
-for i in range(1_000_000):
+for i in range(int(sys.argv[1])):
     random_numbers.append(numpy.random.randint(10, size=10).tolist())    
 
 file = open("./output.json", "w")
